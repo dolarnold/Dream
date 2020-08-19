0b144b59d8f5cfa36f155877eb38a022e609193d
dolarnold <zedekiadol@gmail.com>
2020-08-19 16:43:07 +0300

    search button

--- index.php
+++ index.php
@@ -6,9 +6,49 @@
      <link href="css/owl.carousel.min.css" rel="stylesheet">
      <link href="css/owl.theme.default.min.css" rel="stylesheet">
      <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="fonts/css/all.css" rel="stylesheet">
+     <link href="fonts/css/fontawesome.css"rel="stylesheet">
+     <link href="fonts/css/brands.css"rel="stylesheet">
+     <link href="fonts/css/solid.css" rel="stylesheet">
+     <link href="css/style.css" rel="stylesheet">
  </head>
  <body>
+ <div class="wrapper">
+     <div class="navigation-bar">
+     <div class="container">
+         <!-- insert a logo -->
+         <div class="logo">
+             <img src="images/logo.png" alt="logo">
 
+         </div>
+         <!-- Search box -->
+         <div class="search-area">
+             <form action="" method="post">
+                 <input type="text" name="search_box" class="search_box"placeholder="search all items">
+                 <button class="search-btn btn btn-success"><i class="fa fa-search" aria-hidden="true"></i></button>
+             </form>
+         </div>
+         <!-- User menu -->
+         <div class="user-menu">
+             <ul>
+                 <li>login</li>
+                 <li>signup</li>
+             </ul>
+         </div>
+
+     </div>
+     </div>
+ </div>
+<script src="js/jquery.js"></script>
+ <script src="js/jquery.bxslider.min.js"></script>
+ <script src="js/owl.carousel.js"></script>
+ <script src="js/bootstrap.bundle.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="fonts/js/brands.js"></script>
+ <script src="fonts/js/solid.js"></script>
+ <script src="fonts/js/fontawesome.js"></script>
+ <script src="fonts/js/all.js"></script>
+ <script src="js/main.js"></script>
  </body>
 
  </html>
