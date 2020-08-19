3b82c9920e63936299a06e566fd46acd766b496b
dolarnold <zedekiadol@gmail.com>
2020-08-19 13:31:20 +0300

    js and css files attached

---
+++ index.php
@@ -0,0 +1,14 @@
+<! DOCTYPE html>
+ <html>
+ <head>
+   <title> Dream Ecommerce</title>
+     <link href="css/jquery.bxslider.min.css" rel="stylesheet">
+     <link href="css/owl.carousel.min.css" rel="stylesheet">
+     <link href="css/owl.theme.default.min.css" rel="stylesheet">
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+ </head>
+ <body>
+
+ </body>
+
+ </html>
